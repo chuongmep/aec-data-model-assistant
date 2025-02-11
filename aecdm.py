8e69d9f98042e0657f859103ec3022d6c9664b1d
Petr Broz <petr.broz@autodesk.com>
2025-02-11 23:13:44 +0100

    Get AEC DM schema directly from the GraphQL API.

--- aecdm.py
+++ aecdm.py
@@ -1,21 +1,55 @@
 import os
-import json
 from gql import Client, gql
 from gql.transport.aiohttp import AIOHTTPTransport
 
 API_ENDPOINT = "https://developer.api.autodesk.com/aec/graphql"
+INTROSPECTION_QUERY = """
+{
+  __schema {
+    types {
+      kind
+      name
+      description
+      fields(includeDeprecated: false) {
+        name
+        description
+        args {
+          name
+          description
+          type {
+            kind
+            name
+            ofType {
+              kind
+              name
+            }
+          }
+        }
+        type {
+          kind
+          name
+          ofType {
+            kind
+            name
+          }
+        }
+      }
+    }
+  }
+}
+"""
 ACCESS_TOKEN = os.getenv("APS_ACCESS_TOKEN")
 if not ACCESS_TOKEN:
     raise ValueError("APS_ACCESS_TOKEN environment variable is not set")
 
-with open("schema/aecdm.json", "r") as file:
-    schema = json.load(file)
-    schema_types = schema["data"]["__schema"]["types"]
-
-def get_type_schema(type_name: str) -> dict:
-    return next((item for item in schema_types if item["name"] == type_name), None)
-
 def execute_query(query: str) -> dict:
+    # Create a separate client because tools can be used in parallel
     transport = AIOHTTPTransport(url=API_ENDPOINT, headers={"Authorization": f"Bearer {ACCESS_TOKEN}"})
     client = Client(transport=transport, fetch_schema_from_transport=True)
     return client.execute(gql(query))
+
+schema = execute_query(INTROSPECTION_QUERY)
+types = schema["__schema"]["types"]
+
+def get_type_schema(type_name: str) -> dict:
+    return next((t for t in types if t["name"] == type_name), None)
